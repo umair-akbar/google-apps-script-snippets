cd2d7c70d3e1833c73aebd53a990af458ca11255
Alexander Ivanov <oshli.a.er@gmail.com>
2017-04-05 08:27:05 +0300

    gapps/gilp/notify

--- gapps/gulpfile-notify-send.js
+++ gapps/gulpfile-notify-send.js
@@ -1,4 +1,11 @@
-gulp.task('task', function (cb) {
+/*
+ * Using notify-send
+ **/
+
+var gulp = require('gulp');
+var exec = require('child_process').exec;
+
+gulp.task('default', function (cb) {
   exec('gapps push | while read OUTPUT; do notify-send "$OUTPUT"; done', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
