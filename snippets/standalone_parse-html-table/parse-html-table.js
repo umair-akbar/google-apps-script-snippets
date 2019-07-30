3e90a0f2031139b45e03f23834fa7fa7560103fd
Alexander Ivanov <oshli.a.er@gmail.com>
2019-07-30 08:54:39 +0300

    standalone_parse-html-table
    Signed-off-by: Alexander Ivanov <oshli.a.er@gmail.com>

---
+++ snippets/standalone_parse-html-table/parse-html-table.js
@@ -0,0 +1,69 @@
+/**
+ * @file Parse a html table to the array
+ * @url https://stackoverflow.com/questions/57261523
+ */
+
+/* exported userActionRun */
+
+/**
+ * User action. Runs the snippet
+ */
+function userActionRun() {
+  var content = UrlFetchApp.fetch(
+    'https://www.codelco.com/prontus_codelco/site/edic/base/port/licitaciones_enproceso.html'
+  ).getContentText();
+
+  var data = parseHtmlTableToArray_(content);
+
+  Logger.log(JSON.stringify(data));
+
+  /*
+    // You can also past the data to a spreadsheet
+
+    SpreadsheetApp.openById('XXX')
+    .getSheets()[0]
+    .clearContents()
+    .getRange(1, 1, data.length, data[0].length)
+    .setValues(data);
+  */
+}
+
+/**
+ * Plaseholder
+ * @type {string}
+ */
+var PALSEHOLDER = '-- Better email me: oshli.a.er@gmail.com --';
+
+/**
+ * Parses the first html-table from the content. max 7 columns
+ *
+ * @param {string} content A html string
+ * @returns {Array.<Array.<object>>}
+ */
+function parseHtmlTableToArray_(content) {
+  var inline = content.replace(/[\n\r]/g, ' ');
+  var table = inline.split(/<table.*?>/)[1].split(/<\/table>/)[0];
+
+  return table.split(/<tr.*?>/).map(function(row) {
+    return (
+      row
+        .split(/(<td.*?>|<th.*?>)/)
+        .slice(2)
+        .reduce(function(p, cell, i) {
+          if (~i % 2) {
+            p.push(
+              cell
+                .replace(/<.*?>/g, ' ')
+                .replace(/\s+/g, ' ')
+                .replace(/^\s+/g, '')
+                .replace(/\s+$/g, '')
+            );
+          }
+          return p;
+        }, [])
+        .join(PALSEHOLDER) + PALSEHOLDER.repeat(7)
+    )
+      .split(PALSEHOLDER)
+      .slice(0, 7);
+  });
+}
