86e3850475dd25a21577b2b94223d9484c69fffa
Alexander Ivanov <oshli.a.er@gmail.com>
2019-08-14 21:45:37 +0300

    Updates
    Signed-off-by: Alexander Ivanov <oshli.a.er@gmail.com>

--- snippets/spreadsheet_painting-specific-characters/painting-specific-characters.js
+++ snippets/spreadsheet_painting-specific-characters/painting-specific-characters.js
@@ -20,18 +20,18 @@ function run() {
  * @param {string} color
  */
 function paintingSpecificCharacters_(range, char, color) {
+  var textStyleBuilder = SpreadsheetApp.newTextStyle();
+  textStyleBuilder.setForegroundColor(color);
+  var textStyle = textStyleBuilder.build();
+  var length = char.length;
   var values = range.getValues().map(function(row) {
     return row.map(function(value) {
       var richTextBuilder = SpreadsheetApp.newRichTextValue();
       richTextBuilder.setText(value);
-
-      var textStyleBuilder = SpreadsheetApp.newTextStyle();
-      textStyleBuilder.setForegroundColor(color);
-      var textStyle = textStyleBuilder.build();
       var indexOf = value.indexOf(char);
       while (indexOf > -1) {
-        richTextBuilder.setTextStyle(indexOf, indexOf + 1, textStyle);
-        indexOf = value.indexOf(char, indexOf + 1);
+        richTextBuilder.setTextStyle(indexOf, indexOf + length, textStyle);
+        indexOf = value.indexOf(char, indexOf + length);
       }
       return richTextBuilder.build();
     });
