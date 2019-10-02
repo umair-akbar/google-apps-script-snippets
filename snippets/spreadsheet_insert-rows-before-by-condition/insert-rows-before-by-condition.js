63c906616f0af18c91dc7044881b7a7ea905d284
Alexander Ivanov <oshli.a.er@gmail.com>
2019-10-02 04:48:06 +0300

    insert-rows-before-by-condition
    Signed-off-by: Alexander Ivanov <oshli.a.er@gmail.com>

--- snippets/spreadsheet_insert-rows-before-by-condition/insert-rows-before-by-condition.js
+++ snippets/spreadsheet_insert-rows-before-by-condition/insert-rows-before-by-condition.js
@@ -1,14 +1,14 @@
 /**
  * @file Insert empty rows before by a condition
- * {@link https://stackoverflow.com/a/54731582/1393023}
+ *
  */
 
 /* exported userActionInsertRowsBefore */
 
 /**
- *
+ *{@link https://stackoverflow.com/a/54731582/1393023}
  */
-function userActionInsertRowsBefore() {
+function runSample1() {
   /**
    * @type {conditionCallback}
    */
@@ -24,6 +24,28 @@ function userActionInsertRowsBefore() {
 }
 
 /**
+ *{@link https://stackoverflow.com/a/54731582/1393023}
+ */
+function runSample2() {
+  /**
+   * @type {conditionCallback}
+   */
+  var cb;
+
+  cb = function(row, i, values) {
+    // Returns true if it's not the first row, the first cell of a row does not equal one of previous row
+    // and there is no an empty row before
+    return (
+      values[i - 1] &&
+      values[i - 1][0] !== row[0] &&
+      values[i - 1].join('') !== ''
+    );
+  };
+  var sheet = SpreadsheetApp.getActiveSheet();
+  insertRowBeforeByCondition_(sheet, cb);
+}
+
+/**
  * Insert row before by a condition
  * @param {GoogleAppsScript.Spreadsheet.Sheet} sheet
  * @param {conditionCallback} condition insert or skip the row
@@ -32,9 +54,9 @@ function userActionInsertRowsBefore() {
 function insertRowBeforeByCondition_(sheet, condition) {
   var dataRange = sheet.getDataRange();
   var values = dataRange.getValues();
-  for (var i = values.length - 1; i >= 0; i--) {
+  var i = values.length;
+  while (i-- > 0)
     if (condition(values[i], i, values)) sheet.insertRowBefore(i + 1);
-  }
   return sheet;
 }
 
